1d437a620f311df8cb1773e73ea4ea2ed4e9cf18
Cesarhugocasta <hugocesarch890@gmail.com>
2024-06-01 22:25:43 -0600

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'proyecto';
+  title = 'calculator';
+  result: number=0;
+  calculate (operator: string, operand1: string, operand2: string):void{
+    const num1 = parseFloat (operand1);
+    const num2 = parseFloat (operand2);
+
+    switch(operator){
+      case '+':
+        this.result = num1+num2;
+        break;
+      case '-':
+        this.result = num1-num2;
+        break;
+      case '*':
+        this.result = num1*num2;
+        break;
+      case '/':
+        this.result = num1/num2;
+        break; 
+      default: 
+      this.result=NaN; //Operador no valido.
+    }
+  }
 }
